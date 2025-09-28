525fcbe4b79bb9329c652467c4c385b0f5b2af1f
devreluije <reluije@gmail.com>
2025-09-29 00:14:49 +0200

    tests: add sayHi method test

---
+++ integration-tests/src/test/java/org/galaxy/uniflow/TestAddSayHiMethod.java
@@ -0,0 +1,115 @@
+package org.galaxy.uniflow;
+
+import org.galaxy.uniflow.api.UniClass;
+import org.galaxy.uniflow.api.UniMethod;
+import org.galaxy.uniflow.api.Uniflow;
+import org.galaxy.uniflow.api.elements.UniModifier;
+import org.galaxy.uniflow.api.factories.UniElementFactory;
+import org.galaxy.uniflow.api.factories.UniTypes;
+import org.galaxy.uniflow.api.processing.UniProcessingEnvironment;
+import org.galaxy.uniflow.api.processing.UniProcessor;
+import org.galaxy.uniflow.api.statements.UniVariable;
+import org.galaxy.uniflow.api.types.TypeTag;
+import org.galaxy.uniflow.framework.CompilationHarness;
+import org.galaxy.uniflow.framework.Resource;
+import org.galaxy.uniflow.framework.assertions.AssertStream;
+import org.galaxy.uniflow.junit.IntegrationTest;
+import org.jetbrains.annotations.NotNull;
+
+import javax.lang.model.SourceVersion;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class TestAddSayHiMethod {
+
+    @IntegrationTest
+    public void sayHi(CompilationHarness harness) {
+        String source = """
+                package demo;
+                
+                @TestAnnotation
+                public class Test {
+                
+                    public void run() {
+                        System.out.println("Hello World");
+                    }
+                }
+                """;
+        harness.compile(new AddSayHiProcessor(), new Resource("demo.Test", source), Sources.TEST_ANNOTATION_TYPE)
+                .assertSuccess()
+                .assertClass(Sources.ANNOTATION_NAME)
+                .assertClass("demo.Test", testClass -> {
+                    testClass.assertMethod("run");
+                    testClass.assertMethod("sayHi", sayHi -> {
+                        sayHi.assertReturnType(int.class);
+                        sayHi.assertParameterCount(0);
+                        AssertStream.assertStdout("Hi everyone" + System.lineSeparator(), () -> {
+                            sayHi.assertExecute(1234);
+                        });
+                    });
+                });
+    }
+
+    static class AddSayHiProcessor implements UniProcessor {
+
+        @Override
+        public @NotNull Stream<@NotNull Class<?>> getSupportedAnnotations() {
+            return Stream.empty();
+        }
+
+        @Override
+        public @NotNull Set<@NotNull String> getSupportedAnnotationTypes() {
+            return Set.of(Sources.ANNOTATION_NAME);
+        }
+
+        @Override
+        public @NotNull SourceVersion getSupportedSourceVersion() {
+            return SourceVersion.RELEASE_8;
+        }
+
+        @Override
+        public boolean process(@NotNull Uniflow uniflow, @NotNull UniProcessingEnvironment environment) {
+            UniElementFactory factory = uniflow.getElementFactory();
+
+            if (environment.processingOver()) return true;
+            environment.getElementStreamAnnotatedWithByName(Sources.ANNOTATION_NAME)
+                    .filter(element -> element instanceof UniClass)
+                    .forEach(element -> {
+                        UniClass uniClass = (UniClass) element;
+                        UniVariable thisVar = uniClass.createThis();
+                        UniMethod sayHi = createSayHi(factory, thisVar);
+
+                        uniClass.getMethods().addLast(sayHi);
+                    });
+            return true;
+        }
+
+        private UniMethod createSayHi(UniElementFactory factory, UniVariable thisVar) {
+            return factory.createMethod(
+                    factory.createModifiers(List.of(UniModifier.PUBLIC), List.of()),
+                    "sayHi",
+                    UniTypes.INT,
+                    List.of(),
+                    thisVar,
+                    List.of(),
+                    List.of(),
+                    factory.createBlock(false, List.of(
+                            factory.createExecution(
+                                    factory.createMethodInvocation(
+                                            factory.createFieldAccess(
+                                                    factory.createFieldAccess(System.class, "out"),
+                                                    "println"
+                                            ),
+                                            List.of(),
+                                            List.of(
+                                                    factory.createStringLiteral("Hi everyone")
+                                            )
+                                    )
+                            ),
+                            factory.createReturn(factory.createLiteral(TypeTag.INT, 1234))
+                    ))
+            );
+        }
+    }
+}
