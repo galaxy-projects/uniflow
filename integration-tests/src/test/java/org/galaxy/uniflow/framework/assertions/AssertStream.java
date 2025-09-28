c44a23f160ab4e26583d68c01b329034488f354d
devreluije <reluije@gmail.com>
2025-09-28 23:21:20 +0200

    framework upgrade: - check stdout, stderr - custom test annotation - constant sources for tests

---
+++ integration-tests/src/test/java/org/galaxy/uniflow/framework/assertions/AssertStream.java
@@ -0,0 +1,34 @@
+package org.galaxy.uniflow.framework.assertions;
+
+import org.junit.jupiter.api.Assertions;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.function.Consumer;
+
+public class AssertStream {
+
+    public static void assertStdout(String expected, Runnable task) {
+        PrintStream prevOut = System.out;
+
+        assertStream(expected, task, System::setOut);
+        System.setOut(prevOut);
+    }
+
+    public static void assertStderr(String expected, Runnable task) {
+        PrintStream prevErr = System.err;
+
+        assertStream(expected, task, System::setErr);
+        System.setErr(prevErr);
+    }
+
+    public static void assertStream(String expected, Runnable task, Consumer<PrintStream> setter) {
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            setter.accept(new PrintStream(out));
+            task.run();
+        } catch (IOException e) {
+            Assertions.fail(e);
+        }
+    }
+}
---
+++ integration-tests/src/test/java/org/galaxy/uniflow/junit/IntegrationTest.java
@@ -0,0 +1,15 @@
+package org.galaxy.uniflow.junit;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@ParameterizedTest
+@MethodSource("org.galaxy.uniflow.Sources#createCompilationHarness")
+public @interface IntegrationTest {}
