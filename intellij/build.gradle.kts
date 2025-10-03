452b728257e6f6ef6dab298e2e9b4cdc5330a65e
devreluije <reluije@gmail.com>
2025-10-04 00:17:54 +0200

    module renamed 'intellij' & 'api', 'common' modules imported

--- intellij/build.gradle.kts
+++ intellij/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     id("org.jetbrains.intellij") version "1.17.4"
 }
 
+dependencies {
+    compileOnly(project(":api"))
+    compileOnly(project(":common"))
+}
+
 intellij {
     version.set("2025.1.3")
     type.set("IC")
