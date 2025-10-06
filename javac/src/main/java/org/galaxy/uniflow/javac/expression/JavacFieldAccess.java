66baa8c9bc671820c834fb37b0ec649539841f3e
devreluije <reluije@gmail.com>
2025-10-06 02:41:21 +0200

    field access setters added

--- javac/src/main/java/org/galaxy/uniflow/javac/expression/JavacFieldAccess.java
+++ javac/src/main/java/org/galaxy/uniflow/javac/expression/JavacFieldAccess.java
@@ -3,6 +3,7 @@ package org.galaxy.uniflow.javac.expression;
 import com.sun.tools.javac.tree.JCTree;
 import org.galaxy.uniflow.api.expressions.UniFieldAccess;
 import org.galaxy.uniflow.api.types.UniType;
+import org.galaxy.uniflow.javac.types.JavacExpressionType;
 import org.galaxy.uniflow.javac.util.NameUtils;
 import org.galaxy.uniflow.javac.util.UniflowWrapper;
 import org.jetbrains.annotations.NotNull;
@@ -14,8 +15,22 @@ public class JavacFieldAccess extends JavacExpression<JCTree.JCFieldAccess> impl
     }
 
     @Override
+    public void setSelected(@NotNull UniType selected) {
+        if (!(selected instanceof JavacExpressionType<?, ?>))
+            throw new IllegalArgumentException("Selected type must be JavacExpressionType");
+        JavacExpressionType<?, ?> type = (JavacExpressionType<?, ?>) selected;
+
+        tree.selected = type.getExpression();
+    }
+
+    @Override
     public @NotNull UniType getSelected() {
         return UniflowWrapper.typeFromTree(tree.selected);
+    }
+
+    @Override
+    public void setName(@NotNull String name) {
+        tree.name = NameUtils.name(name);
     }
 
     @Override
