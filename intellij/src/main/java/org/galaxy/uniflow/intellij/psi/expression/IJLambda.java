ebc8fc59f8961c78277c509d14eedd674e795cc5
devreluije <reluije@gmail.com>
2025-10-08 06:13:31 +0200

    IJLambda#setBody implementation

--- intellij/src/main/java/org/galaxy/uniflow/intellij/psi/expression/IJLambda.java
+++ intellij/src/main/java/org/galaxy/uniflow/intellij/psi/expression/IJLambda.java
@@ -1,11 +1,14 @@
 package org.galaxy.uniflow.intellij.psi.expression;
 
+import com.intellij.psi.PsiElementFactory;
 import com.intellij.psi.PsiExpression;
 import com.intellij.psi.PsiLambdaExpression;
 import org.galaxy.uniflow.api.UniElement;
 import org.galaxy.uniflow.api.expressions.UniLambda;
 import org.galaxy.uniflow.api.lists.UniParameterList;
+import org.galaxy.uniflow.intellij.psi.IntellijUniflow;
 import org.galaxy.uniflow.intellij.psi.lists.IJParameterList;
+import org.galaxy.uniflow.intellij.psi.util.IntellijUnwrapper;
 import org.galaxy.uniflow.intellij.psi.util.UniflowWrapper;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,6 +21,21 @@ public class IJLambda extends IJExpression<PsiLambdaExpression> implements UniLa
     @Override
     public @NotNull UniParameterList getParameters() {
         return new IJParameterList(element.getParameterList());
+    }
+
+    @Override
+    public void setBody(@NotNull UniElement body) {
+        if (element.getBody() != null)
+            element.getBody().replace(IntellijUnwrapper.unwrap(body));
+        else {
+            PsiElementFactory factory = IntellijUniflow.getInstance().factory;
+            PsiLambdaExpression dummy = (PsiLambdaExpression) factory.createIdentifier("() -> {}");
+
+            assert dummy.getBody() != null;
+            dummy.getParameterList().replace(element.getParameterList());
+            dummy.getBody().replace(IntellijUnwrapper.unwrap(body));
+            replace(dummy);
+        }
     }
 
     @Override
